3f1096158d0cdb59403e03d36d4a0d456730d677
ikiusen <thiagofcabrera00@gmail.com>
2022-06-03 20:45:04 -0300

    fixed mess

--- image_apache/build-bibliotech.sh
+++ image_apache/build-bibliotech.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
+cd /usr/local/apache2/htdocs && git clone https://github.com/gabrielinuz/bibliotech && chown -R www-data:www-data bibliotech/
 cd htdocs/bibliotech/version2.0/
 chmod +x build.sh
 sh build.sh
-cd /usr/local/apache2/htdocs && chown -R www-data:www-data bibliotech/
